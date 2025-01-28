9e9fef8ee592cd3cd825f0bb66543cf0c52cfe24
drjoeycadieux <instructor.joeycadieux@gmail.com>
2025-01-28 10:50:16 -0500

    Initial commit

---
+++ src/data/blogPosts.js
@@ -0,0 +1,18 @@
+export const blogPosts = [
+  {
+    title: 'Ukraine: A Nation’s Resilience',
+    date: 'January 2025',
+    description:
+      'An overview of the strength and resilience of Ukraine in the face of adversity.',
+  },
+  {
+    title: 'Understanding the Ukrainian Conflict',
+    date: 'December 2024',
+    description: 'A deep dive into the geopolitical issues affecting Ukraine.',
+  },
+  {
+    title: 'Ukraine’s Culture and Heritage',
+    date: 'November 2024',
+    description: 'Exploring the rich culture and history of Ukraine.',
+  },
+];
